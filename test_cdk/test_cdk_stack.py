bab3c3d555cde4659c2a7fca9880696cea7860b7
Emanuel Huber <emanuel.tesv@gmail.com>
2021-03-05 20:08:37 -0300

    Added github actions integration with first s3 bucket

--- test_cdk/test_cdk_stack.py
+++ test_cdk/test_cdk_stack.py
@@ -1,9 +1,9 @@
 from aws_cdk import core
-
+from aws_cdk import aws_s3 as s3
 
 class TestCdkStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        s3.Bucket(self, 'bucket-huber-cdk', bucket_name='bucket-huber-cdk')
